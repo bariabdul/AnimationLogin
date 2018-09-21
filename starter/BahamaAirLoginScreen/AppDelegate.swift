882dc2e5be560e3c0169920e399341806f0abbcd
bariabdul <abdul.bari.1@outlook.com>
2018-09-21 11:13:31 -0400

    Initial commit

---
+++ starter/BahamaAirLoginScreen/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  BahamaAirLoginScreen
+//
+//  Created by Bari Abdul on 9/21/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
